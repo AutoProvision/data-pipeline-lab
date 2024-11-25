9e364250dd096123d28303668ed565224cf0ae56
joaqu1m <joaquim.pires@sptech.school>
2024-11-25 14:12:14 -0300

    yaml load

--- etl/operacoes-credito/handler/yaml_load.py
+++ etl/operacoes-credito/handler/yaml_load.py
@@ -1,26 +1,26 @@
 import yaml
 import os
 
-def load_yaml_as_env(yaml_file):
-    with open(yaml_file, "r") as file:
+YAML_FILE = "config.yaml"
+
+def load_yaml_as_env():
+    with open(YAML_FILE, "r") as file:
         config = yaml.safe_load(file)
 
     def set_env_variables(prefix, data):
         for key, value in data.items():
             if isinstance(value, dict):
-                set_env_variables(f"{prefix}_{key.upper()}", value)
+                set_env_variables(f"{prefix}{'_' if prefix != '' else ''}{key.upper()}", value)
             else:
-                print(f"{prefix}_{key.upper()}")
-                print(str(value))
                 os.environ[f"{prefix}_{key.upper()}"] = str(value)
 
     set_env_variables("", config)
 
-load_yaml_as_env("config.yaml")
+load_yaml_as_env()
 
 database_url = os.getenv("BUCKET_STAGING_NAME")
 api_key = os.getenv("BUCKET_RAW_NAME")
-debug_mode = os.getenv("bucket_refined_name")
+debug_mode = os.getenv("BUCKET_REFINED_NAME")
 
 print(f"Database URL: {database_url}")
 print(f"API Key: {api_key}")
