e1b34904a7aa41dbc5ba02669b173ff6228c09a8
joaqu1m <joaquim.pires@sptech.school>
2024-11-25 21:26:50 -0300

    teste conexão bucket zoho

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+Configurando ambiente da VM
 ```bash
-rm -rf data-pipeline-lab
+sudo apt install docker-compose
+sudo usermod -aG docker $USER
+exit
+```
+
+Iniciando processo
+```bash
+sudo rm -rf data-pipeline-lab
 git clone https://github.com/AutoProvision/data-pipeline-lab.git
 cd data-pipeline-lab
 docker-compose build
--- api/zoho/task1.py
+++ api/zoho/task1.py
@@ -1,5 +1,21 @@
+import os
+import boto3
+
+s3_client = boto3.client('s3')
+
+BUCKET_NAME = os.getenv("BUCKET_RAW_NAME")
+
 def lambda_handler(event, context):
     print("Rodando pipeline zoho...")
+    print("Iniciando teste de conexão ao bucket")
+    print(BUCKET_NAME)
+
+    try:
+        s3_client.list_objects(Bucket=BUCKET_NAME)
+        print("Conexão ao bucket realizada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao conectar ao bucket: {e}")
+
     pass
 
 def handler():
