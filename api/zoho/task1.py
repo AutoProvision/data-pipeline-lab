df25d2baac182e934134abac7a1c58d6685addee
joaqu1m <=>
2024-11-26 12:45:28 -0300

    mesma coisa em 3 camadas, vamos testar

--- api/zoho/task1.py
+++ api/zoho/task1.py
@@ -6,6 +6,9 @@ s3_client = boto3.client('s3')
 BUCKET_NAME = os.getenv("BUCKET_RAW_NAME")
 
 def lambda_handler(event, context):
+
+    print("Task1 Zoho")
+
     print("Rodando pipeline zoho...")
     print("Iniciando teste de conexão ao bucket")
     print(BUCKET_NAME)
@@ -15,8 +18,6 @@ def lambda_handler(event, context):
         print("Conexão ao bucket realizada com sucesso!")
     except Exception as e:
         print(f"Erro ao conectar ao bucket: {e}")
-
-    pass
 
 def handler():
     return lambda_handler({}, {})
