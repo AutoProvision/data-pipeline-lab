d972aeb9012d14887a2b1a4588d4011d28510f13
joaqu1m <=>
2024-11-27 11:59:46 -0300

    mais logs na task1

--- api/operacoes-credito/task1.py
+++ api/operacoes-credito/task1.py
@@ -27,25 +27,24 @@ def find_latest_year():
     return zip_url[len(zip_url) - 8: len(zip_url) - 4]
 
 async def download_and_upload_zip(source_filename, destination_filename):
-    retry_strategy = httpx.Retry(
-        total=5,
-        backoff_factor=0.5,
-        status_forcelist=[500, 502, 503, 504],
-        allowed_methods=["GET"],
-    )
+    bytes_buffer = io.BytesIO()
 
-    transport = httpx.AsyncHTTPTransport(retries=retry_strategy)
-    async with httpx.AsyncClient(timeout=60, transport=transport) as client:
-        chunk_size = 512 * 1024
-        async with client.stream('GET', source_filename) as resp:
-            resp.raise_for_status()
-            bytes_buffer = io.BytesIO()
+    try:
+        async with httpx.AsyncClient(timeout=30) as client:
+            chunk_size = 512 * 1024
+            async with client.stream('GET', source_filename) as resp:
+                resp.raise_for_status()
 
-            async for chunk in resp.aiter_bytes(chunk_size=chunk_size):
-                bytes_buffer.write(chunk)
+                async for chunk in resp.aiter_bytes(chunk_size=chunk_size):
+                    bytes_buffer.write(chunk)
+    except Exception as e:
+        raise f"Erro no download do chunk: {e}"
 
-            bytes_buffer.seek(0)
-            s3_client.upload_fileobj(bytes_buffer, BUCKET_NAME, destination_filename)
+    try:
+        bytes_buffer.seek(0)
+        s3_client.upload_fileobj(bytes_buffer, BUCKET_NAME, destination_filename)
+    except Exception as e:
+        raise f"Erro no upload do chunk: {e}"
 
 def lambda_handler(event, context):
 
