56340f962225ec92ea8e29df88f3d5b507c77755
joaqu1m <=>
2024-11-27 11:13:27 -0300

    tentando pegar credenciais do banco pelo boto3

--- api/zoho/task1.py
+++ api/zoho/task1.py
@@ -2,6 +2,7 @@ import os
 import boto3
 
 s3_client = boto3.client('s3')
+ec2_client = boto3.client('ec2')
 
 BUCKET_NAME = os.getenv("BUCKET_RAW_NAME")
 
@@ -16,6 +17,38 @@ def lambda_handler(event, context):
         print("Conexão ao bucket realizada com sucesso!")
     except Exception as e:
         print(f"Erro ao conectar ao bucket: {e}")
+
+    instance_name = "ec2-autoprovision"
+    filters = [
+        {
+            'Name': 'tag:Name',
+            'Values': [instance_name]
+        }
+    ]
+
+    response = ec2_client.describe_instances(Filters=filters)
+
+    instance_details = None
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_details = {
+                'InstanceId': instance['InstanceId'],
+                'State': instance['State']['Name'],
+                'PublicIpAddress': instance.get('PublicIpAddress', 'No Public IP')
+            }
+
+    if not instance_details:
+        raise RuntimeError(f"Não foi encontrada nenhuma instância com o nome '{instance_name}'.")
+
+    if instance_details['State'] != 'running':
+        raise RuntimeError(
+            f"A instância '{instance_details['InstanceId']}' com o nome '{instance_name}' não está rodando. "
+            f"Estado atual: {instance_details['State']}."
+        )
+
+    print(f"Instance ID: {instance_details['InstanceId']}")
+    print(f"Public IP: {instance_details['PublicIpAddress']}")
 
 def handler():
     return lambda_handler({}, {})
