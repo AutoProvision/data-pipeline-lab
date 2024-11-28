614cb46051e2201162fdea80e6f71542528d085f
joaqu1m <joaqu1m.pires@hotmail.com>
2024-11-27 23:07:18 -0300

    printando uso de memoria

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import importlib.util
 import traceback
 import gc
 from api import yaml_load
+import psutil
+
+def print_memory_usage():
+    process = psutil.Process(os.getpid())
+    print(f"Uso de memória: {process.memory_info().rss / 1024 ** 2:.2f} MB")
 
 def get_files_by_folder(base_path):
     folder_files = {}
@@ -29,18 +34,22 @@ def execute_handlers(base_path, folder_files):
             module_name = f"{folder}.{file[:-3]}"
 
             try:
+                print_memory_usage()
                 spec = importlib.util.spec_from_file_location(module_name, file_path)
                 module = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(module)
+
+                print_memory_usage()
 
                 if hasattr(module, 'handler'):
                     module.handler()
                 else:
                     errors[f"{folder}/{file}"] = "handler() not found"
-                
+                print_memory_usage()
                 del module
                 if module_name in sys.modules:
                     del sys.modules[module_name]
+                print_memory_usage()
 
             except Exception as e:
                 errors[f"{folder}/{file}"] = traceback.format_exc()
